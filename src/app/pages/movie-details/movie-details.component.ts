c220c291c7c0cbee6d4d84329ee1a49ca177b0bf
Fatih Sarıdağ <68101192+fatihsaridag@users.noreply.github.com>
2023-08-09 17:50:18 +0300

    Movie detail has been created

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -10,23 +10,53 @@ import { MovieApiServiceService } from 'src/app/services/movie-api-service.servi
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor(private service : MovieApiServiceService , private router : ActivatedRoute) {
-      
-  }
+  constructor(private service : MovieApiServiceService , private router : ActivatedRoute) {}
+
+  getMovieDetailResult : any;
+  getMovieVideoResult : any;
+  getMovieCastResult : any;
+
 
   ngOnInit(): void {
     let getParamId = this.router.snapshot.paramMap.get('id');
     console.log(getParamId,'getParamid#');
     
-    this.getMovie(getParamId);
-
+    this.getMovie(getParamId);  
+    this.getMovieCast(getParamId);
+    this.getVideo(getParamId);
+            
   }
 
 
   getMovie(id: any){
     this.service.getMovieDetails(id).subscribe((result) => {
       console.log(result , 'getMovieDetails#');
+      this.getMovieDetailResult = result;
     })
+  }
+
+
+  getVideo(id:any)
+  {
+    this.service.getMovieVideo(id).subscribe((result)=>{
+        console.log(result,'getMovieVideo#');
+        result.results.forEach((element:any) => {
+            if(element.type=="Trailer")
+            {
+              this.getMovieVideoResult = element.key;
+            }
+        });
+
+    });
+  }
+
+
+  getMovieCast(id:any)
+  {
+    this.service.getMovieCredit(id).subscribe((result)=>{
+      console.log(result,'movieCast#');
+      this.getMovieCastResult = result.cast;
+    });
   }
 
 
